13d84b5aaa5e98dd000ffa5f197181595b68bd43
Redflame39 <vanya133719@gmail.com>
2021-12-21 23:03:45 +0300

    gift certificate and tag daos

---
+++ src/main/java/com/epam/esm/dao/api/TagDao.java
@@ -0,0 +1,21 @@
+package com.epam.esm.dao.api;
+
+import com.epam.esm.exception.DaoException;
+import com.epam.esm.model.entity.Entity;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TagDao<T extends Entity, K> {
+
+    List<T> findAll() throws DaoException;
+
+    Optional<T> findById(K id) throws DaoException;
+
+    boolean create(T t) throws DaoException;
+
+    boolean update(K updateId, T replacement) throws DaoException;
+
+    boolean delete(K deleteId) throws DaoException;
+
+}
---
+++ src/main/java/com/epam/esm/dao/impl/TagDaoImpl.java
@@ -0,0 +1,49 @@
+package com.epam.esm.dao.impl;
+
+import com.epam.esm.dao.api.TagDao;
+import com.epam.esm.exception.DaoException;
+import com.epam.esm.model.entity.Tag;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Log4j2
+@Repository
+public class TagDaoImpl implements TagDao<Tag, Long> {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public TagDaoImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<Tag> findAll() throws DaoException {
+        return null;
+    }
+
+    @Override
+    public Optional<Tag> findById(Long id) throws DaoException {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean create(Tag tag) throws DaoException {
+        return false;
+    }
+
+    @Override
+    public boolean update(Long updateId, Tag replacement) throws DaoException {
+        return false;
+    }
+
+    @Override
+    public boolean delete(Long deleteId) throws DaoException {
+        return false;
+    }
+}
